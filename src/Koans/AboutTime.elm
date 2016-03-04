f947b8db89e50b3f36b4c39ea85fc4ebf645edda
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-03-05 01:43:54 +0700

    fixed union type

--- src/Koans/AboutTime.elm
+++ src/Koans/AboutTime.elm
@@ -10,20 +10,20 @@ testSuite =
     "About Time"
     [ test
         "time is just a Float"
-        (assertEqual xTime 123.45)
+        (assertEqual 123.45 123.45)
     , test
         "a constant exists for hour"
-        (assertEqual xTime (1 * Time.hour))
+        (assertEqual (Time.hour) (1 * Time.hour))
     , test
         "and minute"
-        (assertEqual xTime (1 * Time.minute))
+        (assertEqual (Time.minute) (1 * Time.minute))
     , test
         "and second"
-        (assertEqual xTime (1 * Time.second))
+        (assertEqual (Time.second) (1 * Time.second))
     , test
         "and millisecond"
-        (assertEqual xTime (1 * Time.millisecond))
+        (assertEqual (Time.millisecond) (1 * Time.millisecond))
     , test
         "helpers exist to convert back to Floats"
-        (assertEqual xNum (Time.inSeconds 1000))
+        (assertEqual 1 (Time.inSeconds 1000))
     ]
